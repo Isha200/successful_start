2dc080f613f2c61a5f6e472abb201f4f76fa37ff
Isha Mitesh Upadhyay <i.upadhyay@se19.qmul.ac.uk>
2022-04-24 12:36:54 +0100

    navbar

---
+++ src/components/Home.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <section id="home">
+                <div className="container">
+                    <div className="row justify-content-center">
+                        <div className="col-md-8">
+                            <h1 className="display-4">
+                                Hi this is isha
+                                hello</h1>
+                            <p className="lead">
+                                Lorem ipsum dolor,
+                                sit amet consectetur adipisicing elit.
+                                Repudiandae eum sapiente accusantium error amet?
+                                Dicta molestias perspiciatis a consectetur delectus repellat omnis.
+                                Quidem possimus accusantium nulla itaque animi ab ipsa?
+
+                            </p>
+
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    )
+}
+
+export default Home
