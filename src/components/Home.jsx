61a08fa4613af7afdc4a058c5eaf42992f32874d
Isha Mitesh Upadhyay <i.upadhyay@se19.qmul.ac.uk>
2022-04-24 13:08:23 +0100

    Home component done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -6,18 +6,16 @@ const Home = () => {
             <section id="home">
                 <div className="container">
                     <div className="row justify-content-center">
-                        <div className="col-md-8">
-                            <h1 className="display-4">
-                                Hi this is isha
-                                hello</h1>
-                            <p className="lead">
-                                Lorem ipsum dolor,
-                                sit amet consectetur adipisicing elit.
-                                Repudiandae eum sapiente accusantium error amet?
-                                Dicta molestias perspiciatis a consectetur delectus repellat omnis.
-                                Quidem possimus accusantium nulla itaque animi ab ipsa?
-
+                        <div className="col-md-8 mt-5">
+                            <h1 className="display-4 fw-bolder mb-4 text-center text-white">
+                               Learning + Networking</h1>
+                            <p className="lead text-center mb-5 text-white">
+                               Want to learn how to start your own business?
                             </p>
+                            <div className="buttons d-flex rounded-pill justify-content-center">
+                                <button className="btn btn-light me-4 rounded-pill px-4 py-2">Login</button>
+                                <button className="btn btn-light me-4 rounded-pill px-4 py-2">Register</button>
+                            </div>
 
                         </div>
                     </div>
