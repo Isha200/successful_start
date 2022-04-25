15e4deb8f5f141a55d10d8e1e3594d786aa34ed5
Isha Mitesh Upadhyay <i.upadhyay@se19.qmul.ac.uk>
2022-04-25 11:58:26 +0100

    log in form finished

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,21 +3,44 @@ import { NavLink } from 'react-router-dom'
 
 const Login = () => {
     return (
-       <div>
-                <div className="container shadow my-5">
+        <div>
+            <div className="container shadow my-5">
                 <div className="row">
-                    <div className="col-md-5 d-flex flex-column align-items-center form">
-                        <h2 className="display-4 fw-bolder">Welcome Back!</h2>
+                    <div className="col-md-5 d-flex flex-column align-items-center justify-content-center form ">
+                        <h2 className="display-4 fw-bolder text-white ">Welcome Back!</h2>
 
-                        <p className="lead text-center">Login with your Email and Password</p>
+                        <p className="lead text-center text-white">Login with your Email and Password</p>
 
-                        <NavLink to="/register" className="btn btn-outline-light rounded-pill pb-2 w-50"></NavLink>
+                        <br></br>
+                        <br></br>
+
+                        <p className="lead text-center text-white">New here?</p>
+                        <NavLink to="/register" className="btn btn-outline-light rounded-pill pb-2 w-50">Register</NavLink>
+                    </div>
+                    <div className="col-md-6 p-5">
+                        <h1 className="display-6 fw-bolder mb-5">Login</h1>
+                        <form>
+                            <div class="mb-3">
+                                <label for="exampleInputEmail1" class="form-label">Email address</label>
+                                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                                <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+                            </div>
+                            <div class="mb-3">
+                                <label for="exampleInputPassword1" class="form-label">Password</label>
+                                <input type="password" class="form-control" id="exampleInputPassword1" />
+                            </div>
+                            <div class="mb-3 form-check">
+                                <input type="checkbox" class="form-check-input" id="exampleCheck1" />
+                                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                            </div>
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </form>
                     </div>
                 </div>
             </div>
 
-            </div>
-            )
+        </div>
+    )
 }
 
-            export default Login
+export default Login
